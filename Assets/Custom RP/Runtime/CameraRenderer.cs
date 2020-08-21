1a6cd5d38d10aa9b39da53e27bac115ab88de5f2
Alekssasho <aleksandar.angelovv@gmail.com>
2020-08-21 10:01:10 +0300

    Initial commit. Unity project with Custom Rendering Pipeline.
    Drawing of skybox and unlit shaders.

---
+++ Assets/Custom RP/Runtime/CameraRenderer.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.Rendering;
+
+public class CameraRenderer
+{
+    ScriptableRenderContext context;
+    Camera camera;
+    const string bufferName = "Render Camera";
+    CommandBuffer buffer = new CommandBuffer
+    {
+        name = bufferName
+    };
+    CullingResults cullingResults;
+
+    static ShaderTagId unlitShaderTagId = new ShaderTagId("SRPDefaultUnlit");
+
+    public void Render(ScriptableRenderContext context, Camera camera)
+    {
+        this.context = context;
+        this.camera = camera;
+
+        if(!Cull())
+        {
+            return;
+        }
+
+        Setup();
+        DrawVisibleGeometry();
+        Submit();
+    }
+
+    bool Cull()
+    {
+        if (camera.TryGetCullingParameters(out ScriptableCullingParameters p))
+        {
+            cullingResults = context.Cull(ref p);
+            return true;
+        }
+        return false;
+    }
+
+    void Setup()
+    {
+        context.SetupCameraProperties(camera);
+        buffer.ClearRenderTarget(true, true, Color.clear);
+        buffer.BeginSample(bufferName);
+        ExecuteBuffer();
+    }
+
+    void Submit()
+    {
+        buffer.EndSample(bufferName);
+        ExecuteBuffer();
+        context.Submit();
+    }
+
+    void DrawVisibleGeometry()
+    {
+        var sortingSettings = new SortingSettings(camera);
+        var drawingSettings = new DrawingSettings(
+            unlitShaderTagId, sortingSettings
+        );
+        var filteringSettings = new FilteringSettings(RenderQueueRange.all);
+
+        context.DrawRenderers(cullingResults, ref drawingSettings, ref filteringSettings);
+
+        context.DrawSkybox(camera);
+    }
+
+    void ExecuteBuffer()
+    {
+        context.ExecuteCommandBuffer(buffer);
+        buffer.Clear();
+    }
+}
